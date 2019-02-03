4f5e9f8c21151b9b2afae9363d299a8e1376aa5c
rolando betara <betara.rolando@gmail.com>
2019-02-03 10:30:53 +0800

    Update index.php
    guide how to use

--- index.php
+++ index.php
@@ -5,9 +5,20 @@
     <?php 
     
         require_once('lib/function.php');
+        
     // The comment is the reference of oop CRUD and relationship database
-        //
-        // 
+
+    //Insert
+        // $sql = "INSERT INTO peronal_informations (f_name, m_name) VALUES ('firstname', 'lastname')";
+        // create($sql);
+
+        //Insert with relationship
+            // $sql = "INSERT INTO peronal_informations (f_name, m_name) VALUES ('firstname', 'lastname')";
+            // $getInsertedID = create($sql);  
+            // $sql1 = "INSERT INTO personal_relationship (pi_id, f_name, m_name) VALUES ('$getInsertedID','firstname', 'lastname')";
+            // create($sql1);
+
+    //Read
         // $sql = "SELECT * FROM peronal_informations";
        
         // foreach (read($sql) as $result)
@@ -15,24 +26,15 @@
         //     echo $result->f_name ;
         //     echo '<br>';
         // }
-        // $obj = new sqlExec;
-       
-        // $obj->dbCUD($sql);
 
-        // $sql = "INSERT INTO peronal_informations (f_name, m_name) VALUES ('t5', 't6')";
-        // $getInsertedID = create($sql);
-        // echo $getInsertedID;   
-        // $sql1 = "INSERT INTO personal_relationship (pi_id, f_name, m_name) VALUES ('$getInsertedID','tg33', 'tg44')";
-        // create($sql1);
+    //Update
+        // $sql = "UPDATE peronal_informations SET f_name = 'update lastname' WHERE id='1'";   
+        // update($sql); 
 
-        // $sql3 = "UPDATE peronal_informations SET f_name = 'updatet5' WHERE id='20'";   
-        // update($sql3);    
-
+    //Delete
         // $sql = "DELETE FROM peronal_informations WHERE id='16'";
         // delete($sql);
 
-        // $getid = getLastID();
-        // echo $getid;
     
     ?>
 <?php require_once('footer.php'); ?>
